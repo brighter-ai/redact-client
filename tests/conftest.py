50f7c712a16c81982fa33b8df72563f9cb0674cb
bjoernwe <bjoernwe@users.noreply.github.com>
2020-10-19 12:26:05 +0200

    [IPS-1804] refactor: Add job fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
+import os
 import pytest
 
 from io import BufferedReader
+
+from ips_job import IPSJob, JobArguments
+
+
+@pytest.fixture
+def ips_url():
+    return os.environ['IPS_URL']
 
 
 @pytest.fixture
 def test_image() -> BufferedReader:
     with open('obama.jpg', 'rb') as f:
         yield f
+
+
+@pytest.fixture
+def job(ips_url, test_image) -> IPSJob:
+    job_args = JobArguments(service='dnat', out_type='images')
+    return IPSJob(file=test_image, job_args=job_args, ips_url=ips_url, start_job=False)
--- tests/test_ips_job.py
+++ tests/test_ips_job.py
@@ -1,54 +1,35 @@
 import io
-import os
 import pytest
 
 from PIL import Image
 
 from ips_data_models import IPSJobState
-from ips_job import JobArguments, IPSJob, ServiceType, OutputType
-
-
-@pytest.fixture
-def ips_url():
-    return os.environ['IPS_URL']
 
 
 class TestIPSJob:
 
-    def test_wait_for_status_completed(self, ips_url, test_image):
-
-        # GIVEN a running IPS instance and a test image
-
-        # WHEN a job is posted
-        job_args = JobArguments(service=ServiceType.dnat, out_type=OutputType.images)
-        job = IPSJob(file=test_image, job_args=job_args, ips_url=ips_url)
-
+    def test_wait_for_status_completed(self, job):
+        # GIVEN an IPS job
+        # WHEN a job is started
         # THEN the job finishes after a while
         assert job.start().wait_until_finished().get_status().state == IPSJobState.finished
 
-    def test_download_result(self, ips_url, test_image):
+    def test_download_result(self, job, test_image):
 
-        # GIVEN a running IPS instance amd a test image
+        # GIVEN a test image
         img = Image.open(test_image)
         test_image.seek(0)
 
-        # WHEN a job is posted
-        job_args = JobArguments(service=ServiceType.dnat, out_type=OutputType.images)
-        job = IPSJob(file=test_image, job_args=job_args, ips_url=ips_url)
-
-        # THEN its result can be downloaded and has the same size as the input image
+        # WHEN a job is started and the result downloaded
         anonymized_raw = job.start().wait_until_finished().download_result()
+
+        # THEN it has the same size as the input image
         anonymized_img = Image.open(io.BytesIO(anonymized_raw))
         assert anonymized_img.size == img.size
 
-    def test_throws_exception_when_not_started(self, ips_url, test_image):
-
-        # GIVEN a running IPS instance and a test image
-
-        # WHEN a job is created but not started
-        job_args = JobArguments(service=ServiceType.dnat, out_type=OutputType.images)
-        job = IPSJob(file=test_image, job_args=job_args, ips_url=ips_url, start_job=False)
-
+    def test_throws_exception_when_not_started(self, job):
+        # GIVEN an IPS job
+        # WHEN the job is not started yet
         # THEN methods throw exceptions
         with pytest.raises(RuntimeError):
             job.get_status()
