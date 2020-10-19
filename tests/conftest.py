de3e49d8c3d400e1ed8a0a52c2c16b8075e2bf81
bjoernwe <bjoernwe@users.noreply.github.com>
2020-10-19 12:45:37 +0200

    [IPS-1804] feat: Test input combinations

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 from io import BufferedReader
 
-from ips_job import IPSJob, JobArguments
+from ips_job import IPSJob, JobArguments, ServiceType, OutputType
 
 
 @pytest.fixture
@@ -17,7 +17,16 @@ def test_image() -> BufferedReader:
         yield f
 
 
+@pytest.fixture(params=[ServiceType.dnat, ServiceType.blur])
+def service(request) -> ServiceType:
+    return request.param
+
+
 @pytest.fixture
-def job(ips_url, test_image) -> IPSJob:
-    job_args = JobArguments(service='dnat', out_type='images')
+def job_args(service) -> JobArguments:
+    return JobArguments(service=service, out_type=OutputType.images)
+
+
+@pytest.fixture
+def job(job_args, ips_url, test_image) -> IPSJob:
     return IPSJob(file=test_image, job_args=job_args, ips_url=ips_url, start_job=False)
