47aa4d6d38cf199b794079ba040a6a215a40010d
bjoernwe <bjoernwe@users.noreply.github.com>
2021-02-03 11:27:57 +0100

    [IPS-2557] Remove async requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from typing import IO
 
 from ips_client.data_models import ServiceType, OutputType
-from ips_client.ips_async_requests import IPSAsyncRequests
 from ips_client.ips_requests import IPSRequests
 from ips_client.job import IPSJob
 from ips_client.settings import Settings
@@ -41,11 +40,6 @@ def service(request) -> ServiceType:
 @pytest.fixture
 def ips(ips_url) -> IPSRequests:
     return IPSRequests(ips_url=ips_url)
-
-
-@pytest.fixture
-def ips_async(ips_url) -> IPSAsyncRequests:
-    return IPSAsyncRequests(ips_url=ips_url)
 
 
 @pytest.fixture
