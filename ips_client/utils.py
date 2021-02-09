9b17244892b99f4f84f242a278434f6f3958f236
bjoernwe <bjoernwe@users.noreply.github.com>
2021-02-09 11:16:38 +0100

    [IPS-2492] feat: Add unit test

--- ips_client/utils.py
+++ ips_client/utils.py
@@ -7,7 +7,6 @@ def normalize_url(url: str):
     if not parse_result.scheme:
         new_url = f'http://{url}'
         assert urllib.parse.urlparse(new_url).scheme == 'http'
-        assert urllib.parse.urlparse(new_url).netloc == url, f'Error with URL: {new_url}'
         return new_url
     return url
 
---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import pytest
+
+from ips_client.utils import normalize_url
+
+
+@pytest.mark.parametrize('input_url, output_url', [
+    ('192.168.11.22', 'http://192.168.11.22'),
+    ('192.168.11.22:42', 'http://192.168.11.22:42'),
+    ('http://192.168.11.22', 'http://192.168.11.22'),
+    ('https://192.168.11.22', 'https://192.168.11.22'),
+    ('foo.org/bar', 'http://foo.org/bar'),
+    ('foo.org/bar/', 'http://foo.org/bar/'),
+])
+def test_normalize_url(input_url: str, output_url: str):
+    assert normalize_url(input_url) == output_url
