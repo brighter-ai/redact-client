67b212122a790bdf6cfbeef2e63b535a0722f16e
bjoernwe <bjoernwe@users.noreply.github.com>
2021-02-12 11:31:11 +0100

    [IPS-2521] feat: Add arguments

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import imread
 
-from ips_client.data_models import ServiceType, OutputType
+from ips_client.data_models import ServiceType, OutputType, JobArguments, Region
 from ips_client.ips_instance import IPSInstance
 
 
@@ -10,8 +10,9 @@ def main():
 
     # anonymize image
     ips = IPSInstance.create(service=ServiceType.blur, out_type=OutputType.images, ips_url='http://127.0.0.1:8787')
+    job_args = JobArguments(region=Region.united_states_of_america)
     with open('tests/resources/obama.jpg', 'rb') as f:
-        job = ips.start_job(file=f)
+        job = ips.start_job(file=f, job_args=job_args)
     result = job.wait_until_finished().download_result()
 
     # show anonymized image
