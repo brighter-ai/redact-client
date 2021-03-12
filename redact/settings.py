93885ed1a2d359f83f8e7d342a16df4e27b4d837
bjoernwe <bjoernwe@users.noreply.github.com>
2021-03-12 16:31:35 +0100

    [IPS-2702] feat: Validate input URLs

--- redact/settings.py
+++ redact/settings.py
@@ -1,7 +1,7 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 
 class Settings(BaseSettings):
     log_level: str = 'INFO'
-    redact_online_url: str = 'https://api.identity.ps/'
-    redact_url_default: str = 'http://127.0.0.1:8787/'
+    redact_online_url: AnyUrl = 'https://api.identity.ps/'
+    redact_url_default: AnyUrl = 'http://127.0.0.1:8787/'
