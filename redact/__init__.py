c92cb619c6f9ed36e0f04edc7b388d653c1b72dc
bjoernwe <bjoernwe@users.noreply.github.com>
2021-03-12 16:00:56 +0100

    [IPS-2702] chore: Bump version

--- redact/__init__.py
+++ redact/__init__.py
@@ -2,7 +2,7 @@
 Python client for "brighter Redact"
 """
 
-__version__ = "v3.7.2"
+__version__ = "v3.8.0"
 
 from .redact_instance import RedactInstance  # noqa
 from .redact_job import RedactJob  # noqa
