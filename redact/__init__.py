8987b7f7eca13e31f11d71f5b010d3c9b68b1ef6
Björn <bjoernwe@users.noreply.github.com>
2021-04-12 15:11:58 +0200

    [IPS-2824] MTS Support (#29)
    * [IPS-2824] feat: Add MTS support
    
    * [IPS-2824] chore: Update version number
    
    * [IPS-2824] chore: Bump version number

--- redact/__init__.py
+++ redact/__init__.py
@@ -2,7 +2,7 @@
 Python client for "brighter Redact"
 """
 
-__version__ = "v3.8.2"
+__version__ = "v3.8.3"
 
 from .redact_instance import RedactInstance  # noqa
 from .redact_job import RedactJob  # noqa
