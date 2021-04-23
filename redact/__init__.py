98665ab1129fa0e5712dd9c980f9d6793da03b7b
bjoernwe <bjoernwe@users.noreply.github.com>
2021-04-23 12:37:46 +0200

    chore: Increase version number

--- redact/__init__.py
+++ redact/__init__.py
@@ -2,7 +2,7 @@
 Python client for "brighter Redact"
 """
 
-__version__ = "v3.8.5"
+__version__ = "v3.8.6"
 
 from .redact_instance import RedactInstance  # noqa
 from .redact_job import RedactJob  # noqa
