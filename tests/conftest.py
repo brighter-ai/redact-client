6fe69b52d8dd7d5acda84b7e27f2252c59d79462
bjoernwe <bjoernwe@users.noreply.github.com>
2021-12-15 12:47:06 +0100

    feature: Add tests for ignore_warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import logging
-import pathlib
 import pytest
 import shutil
 
@@ -40,23 +39,33 @@ def api_key(request) -> str:
     return api_key
 
 
+@pytest.fixture(scope='session')
+def resource_path() -> Path:
+    return Path(__file__).parent.joinpath('resources')
+
+
 @pytest.fixture
-def some_image() -> IO[bytes]:
-    img_path = pathlib.Path(__file__).parent.joinpath('resources/obama.jpg')
+def some_image(resource_path: Path) -> IO[bytes]:
+    img_path = resource_path.joinpath('obama.jpg')
     with open(img_path, 'rb') as f:
         yield f
 
 
 @pytest.fixture
-def some_custom_lp_stamp() -> IO:
-    img_path = pathlib.Path(__file__).parent.joinpath('resources/licence_plate_custom_stamp.png')
+def some_custom_lp_stamp(resource_path: Path) -> IO:
+    img_path = resource_path.joinpath('licence_plate_custom_stamp.png')
     with open(img_path, 'rb') as f:
         yield f
 
 
 @pytest.fixture
 def images_path(tmp_path_factory, some_image, n_images: int = 3) -> Path:
-    """Return a temporary directory that has been prepared with some image files (img_0.jpg, img_1.jpg, ...)."""
+    """
+    Return a temporary directory that has been prepared with some image files:
+    - img_0.jpg,
+    - img_1.jpg,
+    - ...
+    """
     tmp_img_path = tmp_path_factory.mktemp('imgs_dir')
     for i in range(n_images):
         output_path = tmp_img_path.joinpath(f'sub_dir/img_{i}.jpg')
---
+++ tests/functional/test_warnings.py
@@ -0,0 +1,56 @@
+import pytest
+
+from io import FileIO
+from pathlib import Path
+
+from redact import RedactJob, RedactInstance, ServiceType, OutputType, \
+    RedactResponseError
+
+
+@pytest.fixture(scope='session')
+def redact_instance_vid(redact_url: str) -> RedactInstance:
+    return RedactInstance.create(service=ServiceType.blur,
+                                 out_type=OutputType.videos,
+                                 redact_url=redact_url)
+
+
+@pytest.fixture(scope='session')
+def video_with_warning(resource_path: Path) -> FileIO:
+    video_path = resource_path.joinpath('videos/not_starting_with_keyframe.mp4')
+    with open(str(video_path), 'rb') as f:
+        yield f
+
+
+@pytest.fixture(scope='session')
+def job_wo_keyframe(redact_instance_vid: RedactInstance, video_with_warning: FileIO) -> RedactJob:
+    job = redact_instance_vid.start_job(file=video_with_warning)
+    job.wait_until_finished()
+    return job
+
+
+class TestWarnings:
+
+    def test_video_with_missing_key_frame_contains_warning(self, job_wo_keyframe: RedactJob):
+
+        # GIVEN a finished job without keyframe
+        # WHEN its status is downloaded
+        status = job_wo_keyframe.get_status()
+
+        # THEN the status contains a warning
+        assert status.warnings
+
+    def test_download_of_job_with_warnings_is_blocked(self, job_wo_keyframe: RedactJob):
+
+        # GIVEN a finished job without keyframe
+        # WHEN the result is downloaded
+        with pytest.raises(RedactResponseError) as e:
+            job_wo_keyframe.download_result()
+
+            # THEN the download is BLOCKED
+            assert e.value.status_code == 423
+
+    def test_warnings_can_be_ignored_when_downloading(self, job_wo_keyframe: RedactJob):
+        # GIVEN a finished job without keyframe
+        # WHEN the result is downloaded (with ignore_warnings=True)
+        # THEN the download succeeds
+        job_wo_keyframe.download_result(ignore_warnings=True)
