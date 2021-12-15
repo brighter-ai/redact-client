ea934bfac80c5d68ee31f713ac6d707ab169395d
bjoernwe <bjoernwe@users.noreply.github.com>
2021-12-15 11:51:29 +0100

    feature: Update version number

--- redact/__init__.py
+++ redact/__init__.py
@@ -2,7 +2,7 @@
 Python client for "brighter Redact"
 """
 
-__version__ = "v4.4.0"
+__version__ = "4.6.0"
 
 from .redact_instance import RedactInstance  # noqa
 from .redact_job import RedactJob  # noqa
