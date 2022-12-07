5cd5e6d1fef4376fb9d301bb43b08dd1cbd02f9a
kmtran <kieumi.tran94@gmail.com>
2022-12-07 15:52:24 +0100

    fix: tests

--- tests/v3/unit/test_utils.py
+++ tests/v3/unit/test_utils.py
@@ -1,5 +1,8 @@
-import pytest
+import os
+from pathlib import Path
 
+import pytest
+from redact.commons.utils import files_in_dir, images_in_dir, normalize_path
 from redact.utils import normalize_url
 
 
@@ -16,3 +19,39 @@ from redact.utils import normalize_url
 )
 def test_normalize_url(input_url: str, output_url: str):
     assert normalize_url(input_url) == output_url
+
+
+@pytest.mark.parametrize(
+    "in_path, out_path",
+    [
+        ("./foo", str(Path(os.getcwd()).joinpath("foo"))),
+    ],
+)
+def test_normalize_path(in_path: str, out_path: str):
+    assert str(normalize_path(in_path)) == out_path
+
+
+def test_files_in_dir(images_path: Path):
+    # GIVEN a directory with images (ins subfolders)
+    # WHEN the files are listed
+    full_paths = files_in_dir(images_path, recursive=True, sort=True)
+    relative_paths = [str(Path(p).relative_to(images_path)) for p in full_paths]
+    # THEN all images and their subfolders are in the list
+    assert relative_paths == [
+        "sub_dir/img_0.jpg",
+        "sub_dir/img_1.jpg",
+        "sub_dir/img_2.jpg",
+    ]
+
+
+def test_images_in_dir(images_path: Path):
+    # GIVEN a directory with images (ins subfolders)
+    # WHEN the files are listed
+    full_paths = images_in_dir(images_path, recursive=True, sort=True)
+    relative_paths = [str(Path(p).relative_to(images_path)) for p in full_paths]
+    # THEN all images and their subfolders are in the list
+    assert relative_paths == [
+        "sub_dir/img_0.jpg",
+        "sub_dir/img_1.jpg",
+        "sub_dir/img_2.jpg",
+    ]
