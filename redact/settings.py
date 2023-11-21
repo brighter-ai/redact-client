49aae16c78ccecaa3938773ec0aec831c0e77a0d
Patrick <patrick@brighter.ai>
2023-11-21 10:27:43 +0000

    change type to str

--- redact/settings.py
+++ redact/settings.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
-from pydantic import AnyUrl, Field
+from pydantic import Field
 
 
 class Settings(BaseSettings):
     log_level: str = "INFO"
-    redact_online_url: AnyUrl = Field("https://api.brighter.ai/")
-    redact_url_default: AnyUrl = Field("http://127.0.0.1:8787/")
+    redact_online_url: str = "https://api.brighter.ai/"
+    redact_url_default: str = "http://127.0.0.1:8787/"
