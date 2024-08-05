24ecaadf9f29f95b276dc8bf9ad4a1d36cc7eac1
jose.larrain <jnlarrain@uc.cl>
2024-08-05 09:03:39 +0000

    add parameter redact request

--- redact/v4/redact_requests.py
+++ redact/v4/redact_requests.py
@@ -57,11 +57,13 @@ class RedactRequests:
         api_key: Optional[str] = None,
         httpx_client: Optional[httpx.Client] = None,
         custom_headers: Optional[Dict] = None,
+        post_job_timeout: Optional[float] = 60.0,
     ):
         self.redact_url = normalize_url(redact_url)
         self.api_key = api_key
         self.subscription_id = subscription_id
         self.retry_total_time_limit: float = 600  # 10 minutes in seconds
+        self.post_job_timeout = post_job_timeout
 
         self._headers = {"Accept": "*/*"}
         if custom_headers is not None:
@@ -116,7 +118,7 @@ class RedactRequests:
                 files=files,
                 params=job_args.dict(exclude_none=True),
                 headers=self._headers,
-                timeout=60.0,
+                timeout=self.post_job_timeout,
             )
             log.debug(
                 f"Post response to debug id (not output_id) {upload_debug_uuid}: {response}"
