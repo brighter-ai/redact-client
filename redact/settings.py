3c5de8758a733519e827b1ed16ef363bfe305b7d
Radmir Arslanov <radmir.arslanov@brighter.ai>
2024-10-28 09:56:14 +0100

    fix: Make log level upper case
    If log level environment variable set to lower case value,
    it will cause error.

--- redact/settings.py
+++ redact/settings.py
@@ -1,4 +1,4 @@
-from pydantic import AnyUrl, BaseSettings, Field
+from pydantic import AnyUrl, BaseSettings, Field, validator
 
 
 class Settings(BaseSettings):
@@ -6,3 +6,7 @@ class Settings(BaseSettings):
     redact_online_url: AnyUrl = Field("https://api.brighter.ai/")
     redact_url_default: AnyUrl = Field("http://127.0.0.1:8787/")
     base_timeout: int = 60
+
+    @validator("log_level")
+    def log_level_must_be_upper_case(cls, value: str) -> str:
+        return value.upper()
